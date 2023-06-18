ed3b2ae70ff3a6de104790a4df459bcb7f7252aa
StasGil <stanislav.gill@jadexconsulting.com>
2023-06-18 20:21:05 +0300

    Updated wishes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import sound from "./assets/background.mp3";
 import chime from "./assets/chime.mp3";
 import { useState } from "react";
-import wish from "./wish";
+import { newWish } from "./wish";
 
 function App() {
   const [wished, setWish] = useState("");
@@ -11,6 +11,7 @@ function App() {
   const chimes = new Audio(chime);
 
   const handleClick = () => {
+    console.log(newWish.length);
     if ("vibrate" in navigator) {
       // vibration API supported
       window.navigator.vibrate(500);
@@ -25,8 +26,8 @@ function App() {
       return;
     }
     chimes.play();
-    const random = Math.floor(Math.random() * 246);
-    setWish(wish[random]);
+    const random = Math.floor(Math.random() * 224);
+    setWish(newWish[random]);
   };
 
   return (
